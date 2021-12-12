ee674cc4e8df9814d1ac6d1a8582f3f44df9b439
Rakesh rawat <rakesh.rawat.vaishali123@gmail.com>
2021-12-12 11:12:29 +0530

    hello

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-Rakesh:9871315192@cluster0.aaume.mongodb.net/todoListDB",{useNewUrlParser:true});
+mongoose.connect("mongodb://localhost:27017/todoListDB",{useNewUrlParser:true});
 
 const itemSchema={
   name:String
@@ -27,12 +27,6 @@ const item3=new item({
   name:"welcome"
 });
 const defaultItems=[item1,item2,item3];
-item.insertMany(defaultItems,function(err){
-  if(err)
-  console.log(err);
-  else
-  console.log("success");
-});
 app.get("/", function(req, res) {
   item.find({},function(err,foundItems){
     if(foundItems.length===0){
@@ -79,11 +73,10 @@ app.get("/work", function(req,res){
 app.get("/about", function(req, res){
   res.render("about");
 });
-
 let port=process.env.PORT;
-if(port==null||port==""){
-  port=3000;
-}
+ if(port==null||port==""){
+   port=3000;
+ }
 app.listen(port, function() {
   console.log("Server started on port 3000");
 });
