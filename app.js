c658a14d90510bc9cdf8565c04e48a0b943a3072
Rakesh rawat <rakesh.rawat.vaishali123@gmail.com>
2022-01-02 21:10:25 +0530

    fixed mongodb

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const mongoose=require('mongoose');
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const date = require(__dirname + "/date.js");
 
 const app = express();
@@ -11,8 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todoListDB",{useNewUrlParser:true});
-
+mongoose.connect("mongodb+srv://admin-Rakesh:9871315192@cluster0.aaume.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true });
 const itemSchema={
   name:String
 };
